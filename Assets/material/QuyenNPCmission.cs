6c860dafb75a3d7ec8b3cee0694d8b661d49e41a
TripleQ29 <qqq29022000@gmail.com>
2025-10-12 14:53:21 +0700

    NPC1

--- Assets/material/QuyenNPCmission.cs
+++ Assets/material/QuyenNPCmission.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class QuyenNPC-mission : MonoBehaviour
+public class QuyenNPCmission : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
