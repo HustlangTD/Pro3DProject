45a9dddcfc553367244f966c6898387cca1fb82e
HustlangTD <tungdiep125@gmail.com>
2025-10-20 12:20:14 +0700

    update some pakages

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,17 +2,33 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision objectWeHit)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (objectWeHit.gameObject.CompareTag("Enemy"))
         {
-            print("Hit " + collision.gameObject.name + "!");
+            print("Hit " + objectWeHit.gameObject.name + "!");
+            CreateBullerImpactEffect(objectWeHit);
             Destroy(gameObject);
+            
         }
-        if (collision.gameObject.CompareTag("Wall"))
+        if (objectWeHit.gameObject.CompareTag("Wall"))
         {
             print("Hit a wall");
+            CreateBullerImpactEffect(objectWeHit);
             Destroy(gameObject);
         }
+    }
+
+    void CreateBullerImpactEffect(Collision objectWeHit)
+    {
+        ContactPoint contact = objectWeHit.contacts[0];
+
+        GameObject hole = Instantiate(
+            GlobalReferences.Instance.bulletImpactEffectPrefab,
+            contact.point,
+            Quaternion.LookRotation(contact.normal)
+
+        );
+        hole.transform.SetParent(objectWeHit.gameObject.transform);
     }
 }
